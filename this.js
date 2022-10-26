af8698713405170e0c50f410700f643afd2d6287
webhero-nur <nurealom522@gmail.com>
2022-10-26 20:53:34 +0600

    Understand this keyword in JS

---
+++ this.js
@@ -0,0 +1,46 @@
+// console.log(this);
+const kodomAli = {
+    name: 'Kodom Ali',
+    money: 5000,
+    study: 'Math',
+    subjects: ['calculus', 'algebra', 'geometry'],
+    exam: function () {
+        // console.log(this);
+        return `${this.name} is participating in an exam`;
+    },
+    examArrow: () => {
+        console.log(this);
+    },
+    examNested: () => {
+        const arrow = () => {
+            console.log(this);
+        }
+        arrow();
+    },
+    improveExam: function (subject = '') {
+        console.log(this.exam());
+        // this.money = this.money - 10;
+        return `${this.name} is taking improvement exam on ${subject}`;
+    },
+    treatDe: function (amount = 0, tips = 0) {
+        this.money = this.money - amount - tips;
+        return this.money;
+    }
+}
+kodomAli.exam();
+
+const badamAli = {
+    name: 'kacha badam Ali',
+    money: 8000
+}
+
+badamAli.exam = kodomAli.exam;
+badamAli.exam();
+
+function clickHandler() {
+    console.log('inside click handler', this);
+}
+
+document.getElementById('btn-click2').addEventListener('click', function () {
+    console.log(this);
+});
